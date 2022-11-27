d18b2ad6a1839899097f206084c773c412caf57a
deniswebsites <117104632+deniswebsites@users.noreply.github.com>
2022-11-27 04:18:45 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,133 @@
+$(document).ready(function() {
+
+    $(".checkout_list .checkout_val_item").on("click", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".checkout_dr");
+      val = $(this).html();
+      parent.find(".checkout_val .checkout_val").html(val);
+      parent.find("input[type='hidden']").val(val);
+      parent.removeClass("active");
+      parent.find("p").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    $(".dr_title").on("click", function(e) {
+      e.preventDefault();
+      parent = $(this).closest(".dr");
+      if(!parent.hasClass("active")) {
+        if(parent.closest(".dr.active").length == 0) {
+          $(".dr").removeClass("active");
+        }
+        parent.addClass("active");
+      } else {
+        parent.removeClass("active");
+      }
+    });
+
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27) {
+        $(".dr").removeClass("active");
+      }
+    });
+
+    $(document).mouseup(function(e) {
+      hide_element = $(".dr");
+      if (!hide_element.is(e.target)
+          && hide_element.has(e.target).length === 0) {
+          hide_element.removeClass("active");
+        }
+    });
+
+    // ------------
+
+    $("[data-popup-link]").on("click", function(e) {
+        e.preventDefault();
+        popupName = $(this).attr("data-popup-link");
+        div = document.createElement('div');
+        div.style.overflowY = 'scroll';
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+        scrollWidth = div.offsetWidth - div.clientWidth;
+        document.body.removeChild(div);
+        $("body").addClass("fixed");
+        $("body").css({
+            "position" : "fixed",
+            "top" :  -$(document).scrollTop() + "px",
+            "overflow" : "hidden",
+            "right" : 0,
+            "left" : 0,
+            "bottom" : 0,
+            "padding-right" : scrollWidth + "px"
+        });
+        $(".popup_bg").fadeIn(300);
+        $("[data-popup = '"+ popupName +"']").fadeIn(300);
+    });
+    $(".close_popup, .popup_bg").on("click", function(e) {
+        e.preventDefault();
+        curTop = $("body").css("top");
+        curTop = Math.abs(parseInt(curTop, 10));
+        $("body").attr("style", "");
+        if (curTop !== 0) {
+            $("html").scrollTop(curTop);
+        }
+        $("body").removeClass("fixed");
+        $(".popup_bg").fadeOut(300);
+        $("[data-popup]").fadeOut(300);
+    });
+    $(this).keydown(function(eventObject){
+        if (eventObject.which == 27 ) {
+            curTop = $("body").css("top");
+            curTop = Math.abs(parseInt(curTop, 10));
+            $("body").attr("style", "");
+            if (curTop !== 0) {
+                $("html").scrollTop(curTop);
+            }
+            $("body").removeClass("fixed");
+            $(".popup_bg").fadeOut(300);
+            $("[data-popup]").fadeOut(300);
+        }
+    });
+    $(document).on("mouseup", function(e) {
+      if($(".popup").is(":visible")) {
+        e.preventDefault();
+        hide_element = $(".popup_content");
+        if (!hide_element.is(e.target)
+            && hide_element.has(e.target).length === 0) {
+            curTop = $("body").css("top");
+            curTop = Math.abs(parseInt(curTop, 10));
+            $("body").attr("style", "");
+            if (curTop !== 0) {
+                $("html").scrollTop(curTop);
+            }
+            $("body").removeClass("fixed");
+            $(".popup_bg").fadeOut(300);
+            $("[data-popup]").fadeOut(300);
+        }
+      }
+    });
+
+    // --------------
+
+    $(".resp_menu_btn").click(function(e) {
+      e.preventDefault();
+      if( $("#resp_nav").is(":hidden") ) {
+          $("#resp_nav").fadeIn(300);
+          $(this).addClass("active");
+      } else {
+          $("#resp_nav").fadeOut(300);
+          $(this).removeClass("active");
+      }
+    });
+    
+    $(this).keydown(function(eventObject){
+        if (eventObject.which == 27 &&
+            $("#resp_nav").is(":visible") &&
+            bodyWidth <= 767) {
+                $("#resp_nav").fadeOut(300);
+                $(".resp_menu_btn").removeClass("active");
+        }
+    });
+
+});
